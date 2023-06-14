eea3b69eeb4bd094a89b1d28407d0c5e7e99a8a5
Bojan2111 <bojan.adzic@gmail.com>
2023-06-14 18:05:19 +0200

    Update Authentication

--- Bioskop/Controllers/AuthenticationController.cs
+++ Bioskop/Controllers/AuthenticationController.cs
@@ -35,7 +35,7 @@ namespace Bioskop.Controllers
             {
                 UserName = model.Username,
                 Email = model.Email,
-                RegistrationDate = DateTime.Now // Set registration date according to your logic
+                RegistrationDate = DateTime.Now
             };
 
             var result = await _userManager.CreateAsync(user, model.Password);
@@ -76,7 +76,7 @@ namespace Bioskop.Controllers
                 var token = new JwtSecurityToken(
                     issuer: _configuration["Jwt:Issuer"],
                     audience: _configuration["Jwt:Audience"],
-                    expires: DateTime.Now.AddDays(7), // Set token expiration according to your needs
+                    expires: DateTime.Now.AddDays(7),
                     claims: authClaims,
                     signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
                 );
@@ -92,136 +92,5 @@ namespace Bioskop.Controllers
                 return Unauthorized();
             }
         }
-        //private readonly UserManager<ApplicationUser> userManager;
-        //private readonly IConfiguration _configuration;
-
-        //public AuthenticationController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
-        //{
-        //    this.userManager = userManager;
-        //    _configuration = configuration;
-        //}
-
-        //[HttpPost]
-        //[Route("login")]
-        //public IActionResult Login([FromBody] LoginDTO model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest("Login parameters invalid.");
-        //    }
-        //    var user = userManager.FindByNameAsync(model.Username).GetAwaiter().GetResult();
-        //    if (user != null && userManager.CheckPasswordAsync(user, model.Password).GetAwaiter().GetResult())
-        //    {
-
-        //        var authClaims = new List<Claim>
-        //        {
-        //            new Claim(ClaimTypes.Name, user.UserName),
-        //            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-        //        };
-
-        //        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-
-        //        var token = new JwtSecurityToken(
-        //            issuer: _configuration["Jwt:Issuer"],
-        //            audience: _configuration["Jwt:Audience"],
-        //            expires: DateTime.Now.AddHours(2),      // token valid for 2 hours
-        //            claims: authClaims,
-        //            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
-        //        );
-
-        //        return Ok(new TokenDTO()
-        //        {
-        //            Username = user.UserName,
-        //            Token = new JwtSecurityTokenHandler().WriteToken(token),
-        //            Expiration = token.ValidTo
-        //        });
-        //    }
-        //    return Unauthorized();
-        //}
-
-        ////[HttpPost]
-        ////[Route("register")]
-        ////public IActionResult Register([FromBody] RegistrationDTO model)
-        ////{
-        ////    if (!ModelState.IsValid)
-        ////    {
-        ////        return BadRequest("Registration parameters invalid.");
-        ////    }
-
-        ////    var userExists = userManager.FindByNameAsync(model.Username).GetAwaiter().GetResult();
-        ////    if (userExists != null)
-        ////    {
-        ////        return BadRequest("User already exists");
-        ////    }
-
-        ////    ApplicationUser user;
-        ////    if (/* Check if the user is an administrator based on your app's logic */)
-        ////    {
-        ////        user = new ApplicationUser
-        ////        {
-        ////            //Email = model.Email,
-        ////            SecurityStamp = Guid.NewGuid().ToString(),
-        ////            UserName = model.Username
-        ////        };
-
-        ////        var result = userManager.CreateAsync(user, model.Password).GetAwaiter().GetResult();
-        ////        if (!result.Succeeded)
-        ////        {
-        ////            return BadRequest("Validation failed! Please check user details and try again.");
-        ////        }
-
-        ////        userManager.AddToRoleAsync(user, "Administrator").GetAwaiter().GetResult();
-        ////    }
-        ////    else
-        ////    {
-        ////        user = new ApplicationUser
-        ////        {
-        ////            //Email = model.Email,
-        ////            SecurityStamp = Guid.NewGuid().ToString(),
-        ////            UserName = model.Username,
-        ////            DatumRegistracije = DateTime.Now // Set the registration date here
-        ////        };
-
-        ////        var result = userManager.CreateAsync(user, model.Password).GetAwaiter().GetResult();
-        ////        if (!result.Succeeded)
-        ////        {
-        ////            return BadRequest("Validation failed! Please check user details and try again.");
-        ////        }
-
-        ////        userManager.AddToRoleAsync(user, "RegularUser").GetAwaiter().GetResult();
-        ////    }
-
-        ////    return Ok();
-        ////}
-
-        //[HttpPost]
-        //[Route("register")]
-        //public IActionResult Register([FromBody] RegistrationDTO model)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest("Registration parameters invalid.");
-        //    }
-
-        //    var userExists = userManager.FindByNameAsync(model.Username).GetAwaiter().GetResult();
-        //    if (userExists != null)
-        //    {
-        //        return BadRequest("User already exists");
-        //    }
-
-        //    ApplicationUser user = new ApplicationUser()
-        //    {
-        //        SecurityStamp = Guid.NewGuid().ToString(),
-        //        UserName = model.Username,
-        //        DatumRegistracije = model.RegistrationDate
-        //    };
-        //    var result = userManager.CreateAsync(user, model.Password).GetAwaiter().GetResult();
-        //    if (!result.Succeeded)
-        //    {
-        //        return BadRequest("Validation failed! Please check user details and try again.");
-        //    }
-
-        //    return Ok();
-        //}
     }
 }
